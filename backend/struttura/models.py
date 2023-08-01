36c6f6867052e9690919edfb7d7b9b127f7362e4
Francesco Bergonzini <284986@studenti.unimore.it>
2023-08-01 15:08:13 +0200

    Add models for struttura

--- backend/struttura/models.py
+++ backend/struttura/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import F,Q
 
 # Create your models here.
 class Struttura(models.Model):
@@ -21,3 +23,48 @@ class Struttura(models.Model):
         default=False
     )
 
+class Campo(models.Model):
+    # necessaria per creare la chiave composta
+    class Meta:
+        unique_together = (('struttura', 'num_campo'),)
+    
+    struttura = models.OneToOneField(to=Struttura, on_delete=models.CASCADE, primary_key=True, db_column="struttura")
+    num_campo = models.PositiveSmallIntegerField()
+    costo_orario = models.DecimalField(max_digits=3, decimal_places=1)
+    coperto = models.BooleanField(null=True)
+    illuminato = models.BooleanField(null=True)
+
+    class TipiCampo(models.TextChoices):
+      SINTETICO = "sintetico", ("sintetico")
+      NATURALE = "naturale", ("naturale")
+      PARQUET = "parquet", ("parquet")
+      CEMENTO = "cemento", ("cemento")
+      PALESTRA = "palestra", ("palestra")
+    
+    tipo_superficie = models.CharField(
+        choices=TipiCampo.choices,
+        null=True,
+        max_length=9
+    )
+
+class AdminStruttura(models.Model):
+    # vincolo per avere una chiave composta
+    class Meta:
+        unique_together = (
+            ('prenotante', 'struttura'),
+            ('struttura', 'num_campo'),
+        )
+        # vincolo che permette di avere sempre fine > inizio
+        # https://stackoverflow.com/questions/62610053/django-check-constraint-for-datetime
+        constraints = [
+            models.CheckConstraint(
+                check=Q(fine__gt=F('inizio')),
+                name="Controllo orari prenotazione"
+            )
+        ]
+    
+    prenotante = models.OneToOneField(to=User, on_delete=models.CASCADE, primary_key=True)
+    struttura = models.OneToOneField(to=Struttura, on_delete=models.CASCADE)
+    num_campo = models.OneToOneField(to=Campo, on_delete=models.CASCADE)
+    inizio = models.DateTimeField()
+    fine = models.DateTimeField()

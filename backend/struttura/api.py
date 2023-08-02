573c39943f0f3f5401ca7bafcf5856102aa02f4c
Francesco Bergonzini <284986@studenti.unimore.it>
2023-08-02 18:05:27 +0200

    Add basic API for structure info

--- backend/struttura/api.py
+++ backend/struttura/api.py
@@ -1,12 +1,13 @@
 from ninja.router import Router
+from ninja import ModelSchema
 from ninja_jwt.authentication import JWTAuth
-from struttura.models import AdminStruttura
+from struttura.models import AdminStruttura, Struttura
 from django.core.exceptions import ObjectDoesNotExist
 from typing import List
 
 router = Router(tags=['Struttura'])
 
-@router.get('/get_structures/', auth=JWTAuth(), response={200: List[int], 404: None})
+@router.get('/my_structures/', auth=JWTAuth(), response={200: List[int], 404: None})
 def am_admin(request):
     '''
         Restituisce l'id di tutte le strutture di cui l'utente attuale è admin.
@@ -21,3 +22,17 @@ def am_admin(request):
         return 404, None
     except ObjectDoesNotExist:
         return 404, None
+
+
+class SchemaStruttura(ModelSchema):
+    class Config:
+        model = Struttura
+        model_exclude = ['id']
+
+@router.get('/{id}/info/', response={200: SchemaStruttura, 404: str})
+def get_structure_info(request, id:int):
+    try:
+        tmp = Struttura.objects.get(id=id)
+        return 200, tmp
+    except ObjectDoesNotExist:
+        return 404, "La struttura selezionata non esiste."

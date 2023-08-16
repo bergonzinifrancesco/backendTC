0381eabb89048aeb5021fe53cd1fb89afa3dbd35
Francesco Bergonzini <284986@studenti.unimore.it>
2023-08-16 19:47:10 +0200

    Add modify calendar for structure's admins

--- backend/prenotazione/api.py
+++ backend/prenotazione/api.py
@@ -1,10 +1,14 @@
 from ninja.router import Router
 from ninja import ModelSchema
 from ninja_jwt.authentication import JWTAuth
-from struttura.models import Struttura, Campo
+from struttura.models import Struttura, Campo, AdminStruttura
 from prenotazione.models import Prenotazione
 from django.core.exceptions import ObjectDoesNotExist
 from typing import List
+from django.db.models.functions import Now
+from django.contrib.auth.models import User
+
+import datetime
 
 router = Router(tags=["Prenotazione"])
 
@@ -71,3 +75,54 @@ def add_booking(request, booking: AddBookingSchema):
         return 200, "Prenotazione salvata con successo"
     except Exception as e:
         return 400, str(e)
+
+
+@router.post(
+    "/modifica_calendario/{id_struttura}/",
+    response={204: None, 403: str, 404: str, 500: str},
+    auth=JWTAuth(),
+)
+def modifica_calendario(request, id_struttura: int, bookings: List[BookingSchema]):
+    try:
+        try:
+            struttura = Struttura.objects.get(id=id_struttura)
+        except ObjectDoesNotExist:
+            return 404, "Struttura non trovata."
+        except Exception as e:
+            raise e
+        try:
+            AdminStruttura.objects.get(struttura=struttura, admin=request.user)
+        except ObjectDoesNotExist:
+            return 403, "Non sei admin della struttura."
+        except Exception as e:
+            raise e
+
+        # Cancellazione del calendario (vedi frontend)
+        campi = Campo.objects.filter(struttura=struttura)
+        to_be_deleted = Prenotazione.objects.filter(campo__in=campi, inizio__gte=Now())
+        print("to_be_deleted", to_be_deleted)
+        to_be_deleted.delete()
+
+        # Filtraggio degli eventi presentati
+        now = datetime.datetime.now(datetime.timezone.utc)
+        valid_bookings = [
+            b for b in bookings if ((b.fine > b.inizio) and (b.inizio >= now))
+        ]
+
+        # Inserimento degli eventi nel db (uno alla volta)
+        for booking in valid_bookings:
+            try:
+                prenotante = User.objects.get(id=booking.prenotante)
+                campo = Campo.objects.get(id=booking.campo)
+                Prenotazione.objects.create(
+                    prenotante=prenotante,
+                    campo=campo,
+                    inizio=booking.inizio,
+                    fine=booking.fine,
+                )
+            except Exception as e:
+                print(e)
+
+        return 204, None
+    except Exception as e:
+        return 500, str(e)
